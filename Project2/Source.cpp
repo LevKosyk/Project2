86baa6c5c2569ab6e4a07563fd2c314c13e9285e
Lev Kosyk <kosyklev@gmail.com>
2023-10-27 20:30:02 +0300

    Добавьте файлы проекта.

---
+++ Project2/Source.cpp
@@ -0,0 +1,17 @@
+#include"MyMath.h"
+int main() {
+    cout << "PI: " << MyMath::PI() << endl;
+    cout << "+: " << MyMath::add(1, 1) << endl;
+    cout << "-: " << MyMath::sub(4, 1) << endl;
+    cout << "*: " << MyMath::mult(4, 2) << endl;
+    cout << "/: " << MyMath::div(4, 2) << endl;
+    cout << "Rand: " << MyMath::random(1, 10) << endl;
+    cout << "Abs: " << MyMath::abs(-17) << endl;
+    cout << "ceil: " << MyMath::round(17.9) << endl;
+    cout << "floor: " << MyMath::floor(17.9) << endl;
+    cout << "min: " << MyMath::min(4, 2) << endl;
+    cout << "max: " << MyMath::max(4, 2) << endl;
+    cout << "pow: " << MyMath::pow(2, 3) << endl;
+
+    return 0;
+}
